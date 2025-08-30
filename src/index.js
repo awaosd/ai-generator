6533ba0881ff25c6505cd2f362f6a6dc3e34ab51
awaosd <osunadiaz.eva@gmail.com>
2025-08-30 10:48:58 +0100

    improved user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function searchVerse(event) {
   event.preventDefault();
+
   let instructions = document.querySelector("#verse-input");
   let key = "3t7b46o8f5d3a094fe4a456f3ea46695";
   let prompt = `User's instructions are: Generate a Bible verse about ${instructions.value}`;
@@ -7,6 +8,9 @@ function searchVerse(event) {
     "You are a religion expert and love to share knowledge about the Bible. Your mission is to provide only a verse from the bible. Make sure to follow the user's instructions";
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
   axios.get(url).then(giveVerse);
+  let poemElement = document.querySelector("#verse");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = "Generating...";
 }
 
 function giveVerse(response) {
