8909a286b52462dabf09e374cacdb8dbc83ee87e
awaosd <osunadiaz.eva@gmail.com>
2025-08-30 11:06:16 +0100

    change topic and design

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ function searchVerse(event) {
 
   let instructions = document.querySelector("#verse-input");
   let key = "3t7b46o8f5d3a094fe4a456f3ea46695";
-  let prompt = `User's instructions are: Generate a Bible verse about ${instructions.value}`;
+  let prompt = `User's instructions are: Generate a food recipe about ${instructions.value}`;
   let context =
-    "You are a religion expert and love to share knowledge about the Bible. Your mission is to provide only a verse from the bible. Make sure to follow the user's instructions";
+    "You are a food expert and love to share recipes from around the world. Your mission is to provide only a healthy recipe with no comments. Make sure each ingredient of the recipe is separated by a <br />. The title of the recipe should be at the top and using a <strong> element.and a <br /> element to separate it from the ingredients. Make sure to follow the user's instructions";
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
   axios.get(url).then(giveVerse);
   let poemElement = document.querySelector("#verse");
